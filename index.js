8ee13cb322fb5a4535b62344fb0a085590e553b5
robin <kashyap.robin.9277@gmail.com>
2021-10-26 13:33:43 +0530

    restart

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(express.json({ extended: false })); // to access req.body
 
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
+
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/design", require("./routes/api/design"));
 
